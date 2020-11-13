0dbcf3a8664684d6dd38030f5d7f91554d3ac205
Jeferson <jeferson.i.silva@gmail.com>
2020-11-13 03:10:02 -0300

    Protocol bugfix

--- resources/nuxt/plugins/axios.js
+++ resources/nuxt/plugins/axios.js
@@ -1,6 +1,11 @@
+import axios from 'axios';
+
+if (process.browser) {
+    axios.defaults.baseURL = `${window.location.protocol}//${window.location.host}`;
+}
+
+
 // https://axios.nuxtjs.org/
 export default function ({ $axios, redirect }) {
-    if (process.client) {
-        $axios.defaults.baseURL = `${window.location.protocol}//${window.location.host}`;
-    }
+    // 
 }
