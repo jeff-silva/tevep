77cbef15f64b47c6d5c704a551e6c20a354dcfe5
Jeferson Silva <jeferson.i.silva@gmail.com>
2022-05-11 19:43:54 -0300

    admin guard

--- client-nuxt3/middleware/app.global.js
+++ client-nuxt3/middleware/app.global.js
@@ -4,4 +4,9 @@ import { useAppStore } from '@/stores/app';
 export default async (req, res) => {
     const app = useAppStore();
     await app.load();
+
+    // Admin guard
+    if (req.path.startsWith('/admin') && !app.user) {
+        return navigateTo('/auth');
+    }
 };
