c75f4f9c9f9fc8f8e438f764b5a27f853d5258ea
Jeferson Silva <jeferson.i.silva@gmail.com>
2022-06-03 22:32:10 -0300

    Users edit layout

--- nuxt/pages/admin/users.vue
+++ nuxt/pages/admin/users.vue
@@ -19,45 +19,50 @@
                 </template>
 
                 <!-- Edit -->
-                <template #edit-card="crud">
-                    <v-card>
-                        <v-tabs v-model="tab" class="mb-4" background-color="grey-lighten-3">
-                            <v-tab value="home">Dados</v-tab>
-                            <v-tab value="address">Endereço</v-tab>
-                        </v-tabs>
-    
-                        <v-card-text>
+                <template #edit-fields="crud">
+                    <v-row>
+                        <v-col cols="12" md="3">
+                            <app-model-file v-model="crud.edit.photo_id"></app-model-file>
+                        </v-col>
+                        <v-col cols="12" md="9">
+                            <v-tabs v-model="tab" class="mb-4" background-color="grey-lighten-3">
+                                <v-tab value="home">Dados</v-tab>
+                                <v-tab value="address">Endereço</v-tab>
+                                <v-tab value="password">Senha</v-tab>
+                            </v-tabs>
+
                             <v-window v-model="tab">
                                 <v-window-item value="home">
-                                    <v-row>
-                                        <v-col cols="12" md="4">
-                                            <app-model-file v-model="crud.edit.photo_id"></app-model-file>
-                                        </v-col>
-                                        <v-col cols="12" md="8">
-                                            <v-text-field
-                                                label="Nome"
-                                                v-model="crud.edit.name"
-                                                :rules="crud.valid.rules('name', ['required'])"
-                                            ></v-text-field>
-                                            <v-text-field
-                                                label="E-mail"
-                                                v-model="crud.edit.email"
-                                                :rules="crud.valid.rules('email', ['email'])"
-                                            ></v-text-field>
-                                            <app-model-select label="Grupo" v-model="crud.edit.group_id" namespace="users-groups"></app-model-select>
-                                            <div class="d-flex">
-                                                <v-text-field label="Senha" type="password" v-model="crud.edit.password"></v-text-field>
-                                                <v-text-field label="Repita senha" type="password" v-model="crud.edit.password_confirmation" class="ms-2"></v-text-field>
-                                            </div>
-                                        </v-col>
-                                    </v-row>
+                                    <v-text-field
+                                        label="Nome"
+                                        v-model="crud.edit.name"
+                                        :rules="crud.valid.rules('name', ['required'])"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        label="E-mail"
+                                        v-model="crud.edit.email"
+                                        :rules="crud.valid.rules('email', ['email'])"
+                                    ></v-text-field>
+                                    <app-model-select label="Grupo" v-model="crud.edit.group_id" namespace="users-groups"></app-model-select>
                                 </v-window-item>
                                 <v-window-item value="address">
                                     <app-model-place label="Endereço" v-model="crud.edit.address_id"></app-model-place>
                                 </v-window-item>
+                                <v-window-item value="password">
+                                    <v-text-field
+                                        label="Senha"
+                                        type="password"
+                                        v-model="crud.edit.password"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        label="Repita senha"
+                                        type="password"
+                                        v-model="crud.edit.password_confirmation"
+                                    ></v-text-field>
+                                </v-window-item>
                             </v-window>
-                        </v-card-text>
-                    </v-card>
+                        </v-col>
+                    </v-row>
                 </template>
             </app-model-crud>
         </nuxt-layout>
