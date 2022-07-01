d804e4cae717791bdba174c0cbd2e102978f4ff9
Jeferson Silva <jeferson.i.silva@gmail.com>
2022-07-01 11:29:06 -0300

    Nuxt2 bridge + Nuxt3 vuetify beta5

--- nuxt2/nuxt.config.js
+++ nuxt2/nuxt.config.js
@@ -1,9 +1,11 @@
+import { defineNuxtConfig } from '@nuxt/bridge';
 // import colors from 'vuetify/es5/util/colors';
 const { env } = require('../scripts/base');
 
-export default {
+export default defineNuxtConfig({
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  // bridge: false,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -24,20 +26,36 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    // https://go.nuxtjs.dev/config-vuetify
+    ['@nuxtjs/vuetify', {
+      // customVariables: ['~/assets/variables.scss'],
+      // theme: {
+      //   dark: true,
+      //   themes: {
+      //     dark: {
+      //       primary: colors.blue.darken2,
+      //       accent: colors.grey.darken3,
+      //       secondary: colors.amber.darken3,
+      //       info: colors.teal.lighten1,
+      //       warning: colors.amber.base,
+      //       error: colors.deepOrange.accent4,
+      //       success: colors.green.accent3
+      //     }
+      //   }
+      // },
+    }],
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -78,26 +96,6 @@ export default {
     '/files/': { target: env.SERVER_HOST },
   },
 
-  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  // vuetify: {
-  //   customVariables: ['~/assets/variables.scss'],
-  //   theme: {
-  //     dark: true,
-  //     themes: {
-  //       dark: {
-  //         primary: colors.blue.darken2,
-  //         accent: colors.grey.darken3,
-  //         secondary: colors.amber.darken3,
-  //         info: colors.teal.lighten1,
-  //         warning: colors.amber.base,
-  //         error: colors.deepOrange.accent4,
-  //         success: colors.green.accent3
-  //       }
-  //     }
-  //   }
-  // },
-
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+});
